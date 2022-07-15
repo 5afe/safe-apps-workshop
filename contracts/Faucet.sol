72789213b97f0d6ff37d3983b5b4031e11195443
Daniel Somoza <daniel.somoza@safe.global>
2022-07-15 19:20:57 +0200

    Added claim limit and claim cooldown

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -5,22 +5,40 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-uint256 constant claimableAmount = 0.02 ether;
+uint64 constant claimableAmount = 0.02 ether;
+uint64 constant claimableAmountLimit = 0.1 ether;
+uint64 constant claimCooldown = 1 minutes;
 
 /// @title Faucet Contract - A simple faucet contract for the safe apps workshop
 /// @author Daniel Somoza - <daniel.somoza@safe.global>
 contract Faucet is Ownable, Pausable {
-    event FoundsClaimed(address userAddress);
+    event FundsClaimed(address userAddress);
 
-    mapping(address => bool) private claimants;
+    struct Claim {
+        uint64 amount; // total amount claimed
+        uint64 lastClaimTime; // last time claimed
+    }
 
-    modifier onlyCallOnce() {
-        require(!claimants[msg.sender], "You can only claim once");
+    mapping(address => Claim) private claims;
+
+    modifier hasFunds() {
+        require(address(this).balance >= claimableAmount, "No funds available");
         _;
     }
 
-    modifier hasFounds() {
-        require(address(this).balance >= claimableAmount, "No founds prenset");
+    modifier claimLimitNotReached() {
+        require(
+            claims[msg.sender].amount < claimableAmountLimit,
+            "Claim limit reached"
+        );
+        _;
+    }
+
+    modifier isReadyToClaim() {
+        require(
+            claims[msg.sender].lastClaimTime + claimCooldown < block.timestamp,
+            "Claim not ready"
+        );
         _;
     }
 
@@ -28,8 +46,15 @@ contract Faucet is Ownable, Pausable {
         _pause();
     }
 
-    function claimFounds() external whenNotPaused onlyCallOnce hasFounds {
-        claimants[msg.sender] = true;
+    function claim()
+        external
+        whenNotPaused
+        hasFunds
+        claimLimitNotReached
+        isReadyToClaim
+    {
+        claims[msg.sender].amount += claimableAmount;
+        claims[msg.sender].lastClaimTime = uint64(block.timestamp);
         payable(msg.sender).transfer(claimableAmount);
     }
 
